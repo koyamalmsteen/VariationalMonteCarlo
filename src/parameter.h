4730d0501135e6137ee0fb58517cb16729b7728a
root <root@localhost.localdomain>
2012-08-20 21:57:29 +0900

    Added parameter.h

---
+++ src/parameter.h
@@ -0,0 +1,28 @@
+/* phys_conditions */
+#define JIGEN 1
+#define TOTAL_SITE_NUMBER 20
+#define TOTAL_UP_ELECTRON 10
+#define TOTAL_DOWN_ELECTRON 10
+#define TRANSFER -1
+#define PROJECTION 0          /* 0:normal H, 1:projected H */
+/* math_conditions */
+#define INIT_WAVE_VECTOR 2    /* 0:PLANE , 1:HF 2:PLANE+FIELD */
+#define GAMMA_ALGORITHM 0
+#define PERIODIC 0
+
+#define RANDOM_ALGORITHM 0
+#define HF_INITIAL_RHO 0
+#define TRASH 10
+#define MAX_HF_ITERATION 10000
+#define HF_CONVERGENCE_CONDITION 0.0000000001
+
+#define COR 1
+
+#define POWER 0
+#define NUMBER_XI 1
+#define MAX_MONTECARLO_SAMPLE 100
+#define MINIMUM 0
+#define FLAT_BAND 0 
+
+#define NU 1
+#define PERTUR 0
